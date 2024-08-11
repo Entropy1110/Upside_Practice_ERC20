94048011c0312eeb4571d8b577ab15f7e7d4fcb4
Entropy1110 <entropy1110@gmail.com>
2024-08-11 16:02:31 +0900

    modified:   src/ERC20.sol 	src/Context.sol 	src/ECDSA.sol 	src/EIP712.sol 	src/MessageHashUtils.sol 	src/Nonces.sol 	src/Pausable.sol 	src/ShortStrings.sol 	src/StorageSlot.sol

---
+++ src/ERC20.sol
@@ -0,0 +1,228 @@
+pragma solidity ^0.8.20;
+
+import {Context} from "./Context.sol";
+import "./Pausable.sol";
+import {EIP712} from "./EIP712.sol";
+import {ECDSA} from "./ECDSA.sol";
+import {Nonces} from "./Nonces.sol";
+import {MessageHashUtils} from "./MessageHashUtils.sol";
+
+
+contract ERC20 is Context, Pausable, Nonces, EIP712{
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    bytes32 private constant TYPE_HASH =
+        keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
+    bytes32 private constant PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+
+    error ERC2612ExpiredSignature(uint256 deadline);
+
+    error ERC20InvalidSender(address addr);
+    error ERC20InvalidReceiver(address addr);
+    error ERC20InsufficientBalance(address from, uint fromBalance, uint value);
+    error ERC20InvalidApprover(address addr);
+    error ERC20InvalidSpender(address addr);
+    error ERC20InsufficientAllowance(address spender, uint current, uint value);
+
+
+
+
+
+    mapping(address account => uint256) private _balances;
+
+    mapping(address account => mapping(address spender => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+
+    bytes32 private immutable _hashedName;
+    bytes32 private immutable _hashedVersion;
+
+    address private ERC_owner;
+
+    constructor(string memory name_, string memory symbol_) EIP712(name_, "1") {
+        _name = name_;
+        _hashedName = keccak256(bytes(_name));
+        _hashedVersion = keccak256(bytes("1"));
+        _symbol = symbol_;
+        ERC_owner = msg.sender;
+        _mint(msg.sender, 100 ether);
+        _mint(address(this), 500 ether);
+
+    }
+
+    function name() public view virtual returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view virtual returns (uint8) {
+        return 18;
+    }
+
+    function totalSupply() public view virtual returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view virtual returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 value) public virtual returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, value);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view virtual returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 value) public virtual returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public virtual returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, value);
+        _transfer(from, to, value);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 value) internal {
+        if (from == address(0)) {
+            revert ERC20InvalidSender(address(0));
+        }
+        if (to == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        _update(from, to, value);
+    }
+
+    function _update(address from, address to, uint256 value) internal virtual whenNotPaused {
+        if (from == address(0)) {
+            _totalSupply += value;
+        } else {
+            uint256 fromBalance = _balances[from];
+            if (fromBalance < value) {
+                revert ERC20InsufficientBalance(from, fromBalance, value);
+            }
+            unchecked {
+                _balances[from] = fromBalance - value;
+            }
+        }
+
+        if (to == address(0)) {
+            unchecked {
+                _totalSupply -= value;
+            }
+        } else {
+            unchecked {
+                _balances[to] += value;
+            }
+        }
+
+        emit Transfer(from, to, value);
+    }
+
+    function _mint(address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        _update(address(0), account, value);
+    }
+
+    function _burn(address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert ERC20InvalidSender(address(0));
+        }
+        _update(account, address(0), value);
+    }
+
+    function _approve(address owner, address spender, uint256 value) internal {
+        _approve(owner, spender, value, true);
+    }
+
+    function _approve(address owner, address spender, uint256 value, bool emitEvent) internal virtual {
+        if (owner == address(0)) {
+            revert ERC20InvalidApprover(address(0));
+        }
+        if (spender == address(0)) {
+            revert ERC20InvalidSpender(address(0));
+        }
+        _allowances[owner][spender] = value;
+        if (emitEvent) {
+            emit Approval(owner, spender, value);
+        }
+    }
+
+    function _spendAllowance(address owner, address spender, uint256 value) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance != type(uint256).max) {
+            if (currentAllowance < value) {
+                revert ERC20InsufficientAllowance(spender, currentAllowance, value);
+            }
+            unchecked {
+                _approve(owner, spender, currentAllowance - value, false);
+            }
+        }
+    }
+
+    function pause() public {
+        if (_msgSender() == ERC_owner){
+            _pause();
+        }
+        else{
+            revert("not an owner");
+        }
+    }
+
+    
+
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public virtual {
+        if (block.timestamp > deadline) {
+            revert ERC2612ExpiredSignature(deadline);
+        }
+
+        bytes32 structHash = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, _useNonce(owner), deadline));
+
+        bytes32 hash = _hashTypedDataV4(structHash);
+
+        address signer = ECDSA.recover(hash, v, r, s);
+        if (signer != owner) {
+            revert("INVALID_SIGNER");
+        }
+
+        _approve(owner, spender, value);
+    }
+
+
+
+    function DOMAIN_SEPARATOR() external view virtual returns (bytes32) {
+        return _domainSeparatorV4();
+    }
+
+    function _toTypedDataHash(bytes32 structHash) public view returns (bytes32){
+        return _hashTypedDataV4(structHash);
+    }
+
+}
