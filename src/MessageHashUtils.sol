8b08d7d6437a1c01525840492bbdba253fb206ad
Entropy1110 <entropy1110@gmail.com>
2024-08-11 16:03:17 +0900

    new file:   src/Context.sol 	new file:   src/ECDSA.sol 	new file:   src/EIP712.sol 	new file:   src/MessageHashUtils.sol 	new file:   src/Nonces.sol 	new file:   src/Pausable.sol 	new file:   src/ShortStrings.sol 	new file:   src/StorageSlot.sol

---
+++ src/MessageHashUtils.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.20;
+
+
+library MessageHashUtils {
+
+function toTypedDataHash(bytes32 domainSeparator, bytes32 structHash) internal pure returns (bytes32 digest) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let ptr := mload(0x40)
+            mstore(ptr, hex"19_01")
+            mstore(add(ptr, 0x02), domainSeparator)
+            mstore(add(ptr, 0x22), structHash)
+            digest := keccak256(ptr, 0x42)
+        }
+    }
+}
